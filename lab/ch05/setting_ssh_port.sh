1e84918744881ffc963071d981fcc26c70bf8ede
Jonny Lai <jonny.lai@commandp.com>
2016-12-05 01:19:12 +0800

    finish the quick switch ssh port script

---
+++ lab/ch05/setting_ssh_port.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ============================================================
+#  Author: Chu-Siang Lai / chusiang (at) drx.tw
+#  Blog: http://note.drx.tw
+#  Filename: setting_ssh_port.sh
+#  Modified: 2016-12-05 00:53
+#  Description: Quick replace ssh port for CH05 lab.
+# =========================================================== 
+
+# Get original ssh port.
+SSH_PORT_ORI=$( \
+    grep ansible_ssh_port hosts | \
+    awk '{ print $3 }' | \
+    sed 's/ansible_ssh_port=//'
+  )
+
+# Get ssh port.
+SSH_PORT_NEW=$( \
+    docker ps | \
+    grep server1 | \
+    awk '{ print $12 }' | \
+    sed 's/0.0.0.0://;s/->22\/tcp//'
+  )
+
+# Switch ssh port in inventory file.
+sed "s/$SSH_PORT_ORI/$SSH_PORT_NEW/" hosts > hosts.tmp && \
+  mv hosts.tmp hosts
+
