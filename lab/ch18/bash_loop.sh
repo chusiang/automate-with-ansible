320895599dec212b89618c189e3b163959059577
Jonny Lai <jonny.lai@commandp.com>
2016-12-19 00:49:17 +0800

    Day18 release !
    看來只好等鐵人賽結束才能玩新出 loop 語法了。

---
+++ lab/ch18/bash_loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ============================================================
+#  Author: Chu-Siang Lai / chusiang (at) drx.tw
+#  Blog: http://note.drx.tw
+#  Filename: bash_loop.sh
+#  Modified: 2016-12-18 20:15
+#  Description: A sample for loop with bash script.
+#  Reference:
+#
+#   1. Bash For Loop Examples | nixCraft
+#    - https://www.cyberciti.biz/faq/bash-for-loop/
+#
+# ============================================================
+
+for X in 0 1 2; do
+  echo Loop $X
+done
